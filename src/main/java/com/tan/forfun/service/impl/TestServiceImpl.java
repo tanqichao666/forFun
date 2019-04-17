588f7f062f54e7adabc6aa932a7811a36f120300
tanqichao666 <ictors@163.com>
2019-04-17 17:08:15 +0800

    除末尾节点其他节点都计算

--- src/main/java/com/tan/forfun/service/impl/TestServiceImpl.java
+++ src/main/java/com/tan/forfun/service/impl/TestServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import com.tan.forfun.dao.TestDao;
@@ -32,18 +33,18 @@ public class TestServiceImpl implements TestService {
 		if(null != treeModel) {
 			List<TreeModel> arrayList = new ArrayList<>();
 			List<TreeModel> children = treeModel.getChildren();
-			for (TreeModel treeModel2 : children) {
-				if(StringUtils.isEmpty(treeModel2.getMoney())) {
+			if(!CollectionUtils.isEmpty(children)) {
+				for (TreeModel treeModel2 : children) {
 					calcTree(treeModel2);
+					arrayList.add(treeModel2);
 				}
-				arrayList.add(treeModel2);
+				int intValue = arrayList.stream().map(map -> {
+					if(!StringUtils.isEmpty(map.getMoney())) {
+						return Integer.parseInt(map.getMoney());
+					} return 0;
+				}).reduce(0,(a,b) ->  a+b).intValue();
+				treeModel.setMoney(String.valueOf(intValue));
 			}
-			int intValue = arrayList.stream().map(map -> {
-				if(!StringUtils.isEmpty(map.getMoney())) {
-					return Integer.parseInt(map.getMoney());
-				} return 0;
-			}).reduce(0,(a,b) ->  a+b).intValue();
-			treeModel.setMoney(String.valueOf(intValue));
 		}
 	}
 
